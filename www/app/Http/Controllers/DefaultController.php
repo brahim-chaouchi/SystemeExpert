517f0de1aabdccd432b724103b52f0d047c5b27c
brahim <brahim.chaouchi@hotmail.fr>
2018-09-06 14:05:13 +0200

    separe les routes index get/post

--- www/app/Http/Controllers/DefaultController.php
+++ www/app/Http/Controllers/DefaultController.php
@@ -9,7 +9,17 @@ use Illuminate\Support\Facades\Session;
 
 class DefaultController extends Controller
 {
-    public function index(Request $argRequest){
+    public function indexGet(){
+        Session::forget('reponse');
+        //prendre la question qui touche le plus de problemes
+        $questionId = DB::select(DB::raw('select Question.id, count(Probleme.id) as nb from Question join Reponse on Reponse._Question=Question.id join Resultat on Resultat._Reponse=Reponse.id join Probleme on Resultat._Probleme=Probleme.id group by Question.id order by nb,id'));
+        $question=QuestionModel::find($questionId[0]->id);
+        return view('devine', compact('question'));
+    }
+
+    public function indexPost(Request $argRequest){
+        Session::push('reponse', $argRequest->input('reponse'));
+        $reponse=Session::get('reponse');
         //prendre la question qui touche le plus de problemes
         $questionId = DB::select(DB::raw('select Question.id, count(Probleme.id) as nb from Question join Reponse on Reponse._Question=Question.id join Resultat on Resultat._Reponse=Reponse.id join Probleme on Resultat._Probleme=Probleme.id group by Question.id order by nb,id'));
         $question=QuestionModel::find($questionId[0]->id);
--- www/routes/web.php
+++ www/routes/web.php
@@ -11,7 +11,8 @@
 |
 */
 
-Route::any('/', "DefaultController@index")->name("index");
+Route::get('/', "DefaultController@indexGet")->name("index");
+Route::post('/', "DefaultController@indexPost");
 Route::get('/Question/new', "QuestionController@new1")->name("question.new");
 Route::post('/Question/new', "QuestionController@new2");
 Route::get('/Question/list', "QuestionController@list1")->name("question.list");
